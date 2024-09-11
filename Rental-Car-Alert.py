9d2827c696da39e0c1b774d9245c8570d04dd977
Adrian Alvarez-Dell <adrianalvarez3091@gmail.com>
2024-09-11 16:18:08 +0200

    fix bug

--- Rental-Car-Alert.py
+++ Rental-Car-Alert.py
@@ -51,101 +51,109 @@ old_results = 0
 
 
 while True:
-    print('... Searching in DoyouSpain...')
-    text = ''
-    email = False
-    browser = webdriver.Chrome(options=options)#open headless chrome1
-    browser.get(url)
-    time.sleep(1)
     try:
-        # Locate the "Aceptar todas las cookies" button using its ID
-        accept_cookies_button = browser.find_element(By.ID, "checkAllOptions")
-        
-        # Click the button to accept cookies
-        accept_cookies_button.click()
-        print("Cookies accepted.")
-
-    except Exception as e:
-        print(f"Error: {e}")
-  
-    try:
-        fuel_option = WebDriverWait(browser, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, '.fuel-option.fuel-option-none.tooltipBlancoBig[title*="Lleno/Lleno"]'))
-        )
-        fuel_option.click()
-        print('Fuel option set to Full/Full')
-    except Exception as e:
-        print(f"Error: {e}")
-            
-    time.sleep(3)
-    if only_cancelable:
+        print('... Searching in DoyouSpain...')
+        text = ''
+        email = False
+        browser = webdriver.Chrome(options=options)#open headless chrome1
+        browser.get(url)
+        time.sleep(1)
         try:
-            cancel_option = WebDriverWait(browser, 10).until(
-                EC.element_to_be_clickable((By.ID, 'idCancel1'))
-            )
+            # Locate the "Aceptar todas las cookies" button using its ID
+            accept_cookies_button = browser.find_element(By.ID, "checkAllOptions")
             
-            # Click the radio button if it's present
-            cancel_option.click()
-            print("Only cancelable cars selected.")
-        
+            # Click the button to accept cookies
+            accept_cookies_button.click()
+            print("Cookies accepted.")
+
         except Exception as e:
             print(f"Error: {e}")
-    time.sleep(3)
-
-    page_source = browser.page_source
-    soup = BeautifulSoup(page_source, features="lxml")
-    #find a list of each soup per car:
-    soups = get_car_soups(soup)
-    results = {}
-    for n, car in enumerate(soups):
-        results[n] = get_info_car(car, n, limit)
-    filtered_results ={}
-    # print(results)
-    for nn, result in enumerate(results.values()):
-        print(result[4], result[0], 'limit:',limit*insurance_ratio)
-        if (result[0]< limit*insurance_ratio) and result[4] != 'Lleno/Vacío (Dev.)':
-            print('checking insurance price...')
-            filtered_results[nn] = result
-            time.sleep(1)
-            element = browser.find_element(By.CLASS_NAME,"bdg-container")
-            # Execute JavaScript code to set the element's style attribute to "display:none"
-            browser.execute_script("arguments[0].style.display='none'", element)
-            WebDriverWait(browser, 90).until(EC.element_to_be_clickable((By.NAME,str('coche'+str(nn+1))))).click()
-            new_page_source = browser.page_source
-            newsoup = BeautifulSoup(new_page_source, features="lxml")
-            filtered_results[nn] += [get_insurance_price(newsoup, result[0])]
-            browser.back()
     
-    browser.close()
-    if insurance_limit:
-        email_results = {}
-        for result in filtered_results:
-            if filtered_results[result][-1] < limit:
-                email_results[result] = filtered_results[result]
-    else:
-        email_results = filtered_results
-    email = True if len(email_results) > 0 and str(old_results).replace("'","").replace('"', '') != str(email_results).replace("'","").replace('"', '')  else False
-    old_results = email_results.copy()
-    if email:
-        print(len(email_results), 'results found under the setted price')
-        subject = str(len(email_results)) + ' Car(s) at lower price in DoyouSpain'
-        text = email_text_generator(email_results, insurance_limit, limit, url)
-        sendedmail = sendemail(emailadress, subject, text)
-        if sendedmail:
-            print('...Email sent...')
-            sendedmail = False
-        # conn.quit()     
-    elif len(email_results) < 1:
-        print("No cars found cheaper than", limit, '€')
-    else:
-        print('Results have been already notified by email in the previous lookup')
-    waitingfor = 60*60*random.uniform(0.7, 0.9)*1
-    print("Waiting for next round: ", waitingfor/60/60)
-    now = dt.datetime.now()
-    t1 = dt.datetime.strptime(str(now.hour)+':'+str(now.minute) +':0', '%H:%M:%S')
-    t2 = dt.datetime.strptime(str(int(waitingfor/3600))+':'+str(int((waitingfor/3600 % 1*60))) +':0', '%H:%M:%S')
-    time_zero = dt.datetime.strptime('00:00:00', '%H:%M:%S')
-    print('Next search programmed at:',(t1 - time_zero + t2).time())
-    time.sleep(waitingfor)
+        try:
+            fuel_option = WebDriverWait(browser, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, '.fuel-option.fuel-option-none.tooltipBlancoBig[title*="Lleno/Lleno"]'))
+            )
+            fuel_option.click()
+            print('Fuel option set to Full/Full')
+        except Exception as e:
+            print(f"Error: {e}")
+                
+        time.sleep(3)
+        if only_cancelable:
+            try:
+                cancel_option = WebDriverWait(browser, 10).until(
+                    EC.element_to_be_clickable((By.ID, 'idCancel1'))
+                )
+                
+                # Click the radio button if it's present
+                cancel_option.click()
+                print("Only cancelable cars selected.")
+            
+            except Exception as e:
+                print(f"Error: {e}")
+        time.sleep(3)
 
+        page_source = browser.page_source
+        soup = BeautifulSoup(page_source, features="lxml")
+        #find a list of each soup per car:
+        soups = get_car_soups(soup)
+        results = {}
+        for n, car in enumerate(soups):
+            results[n] = get_info_car(car, n, limit)
+        filtered_results ={}
+        # print(results)
+        for nn, result in enumerate(results.values()):
+            print(result[4], result[0], 'limit:',limit*insurance_ratio)
+            if (result[0]< limit*insurance_ratio) and result[4] != 'Lleno/Vacío (Dev.)':
+                print('checking insurance price...')
+                filtered_results[nn] = result
+                time.sleep(1)
+                element = browser.find_element(By.CLASS_NAME,"bdg-container")
+                # Execute JavaScript code to set the element's style attribute to "display:none"
+                browser.execute_script("arguments[0].style.display='none'", element)
+                WebDriverWait(browser, 90).until(EC.element_to_be_clickable((By.NAME,str('coche'+str(nn+1))))).click()
+                new_page_source = browser.page_source
+                newsoup = BeautifulSoup(new_page_source, features="lxml")
+                filtered_results[nn] += [get_insurance_price(newsoup, result[0])]
+                browser.back()
+        
+        browser.close()
+        if insurance_limit:
+            email_results = {}
+            for result in filtered_results:
+                if filtered_results[result][-1] < limit:
+                    email_results[result] = filtered_results[result]
+        else:
+            email_results = filtered_results
+        email = True if len(email_results) > 0 and str(old_results).replace("'","").replace('"', '') != str(email_results).replace("'","").replace('"', '')  else False
+        old_results = email_results.copy()
+        if email:
+            print(len(email_results), 'results found under the setted price')
+            subject = str(len(email_results)) + ' Car(s) at lower price in DoyouSpain'
+            text = email_text_generator(email_results, insurance_limit, limit, url)
+            sendedmail = sendemail(emailadress, subject, text)
+            if sendedmail:
+                print('...Email sent...')
+                sendedmail = False
+            # conn.quit()     
+        elif len(email_results) < 1:
+            print("No cars found cheaper than", limit, '€')
+        else:
+            print('Results have been already notified by email in the previous lookup')
+        waitingfor = 60*60*random.uniform(0.7, 0.9)*1
+        print("Waiting for next round: ", waitingfor/60/60)
+        now = dt.datetime.now()
+        t1 = dt.datetime.strptime(str(now.hour)+':'+str(now.minute) +':0', '%H:%M:%S')
+        t2 = dt.datetime.strptime(str(int(waitingfor/3600))+':'+str(int((waitingfor/3600 % 1*60))) +':0', '%H:%M:%S')
+        time_zero = dt.datetime.strptime('00:00:00', '%H:%M:%S')
+        print('Next search programmed at:',(t1 - time_zero + t2).time())
+        time.sleep(waitingfor)
+    except Exception as e:
+        print(f"Error: {e}")
+        try:
+            browser.close()
+        except:
+            pass
+        time.sleep(60*5)
+        continue
 
